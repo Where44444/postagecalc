5106d914f4bdcb5e61e0fa142ff320d3a96948c9
Alex Charles <alexjob44@gmail.com>
2018-06-19 18:55:08 -0600

    Message

--- index.js
+++ index.js
@@ -27,31 +27,107 @@ function handleMath(request, response) {
 
 	var operation = requestUrl.query.operation;
 	var operand1 = Number(requestUrl.query.operand1);
-	var operand2 = Number(requestUrl.query.operand2);
 
-	computeOperation(response, operation, operand1, operand2);
+	computeOperation(response, operation, operand1);
 }
 
-function computeOperation(response, op, left, right) {
+function computeOperation(response, op, weight) {
 	op = op.toLowerCase();
 
 	var result = 0;
 
-	if (op == "add") {
-		result = left + right;
-	} else if (op == "subtract") {
-		result = left - right;
-	} else if (op == "multiply") {
-		result = left * right;
-	} else if (op == "divide") {
-		result = left / right;
-	} else {
-		// It would be best here to redirect to an "unknown operation"
-		// error page or something similar.
+	if (op == "letters (stamped)") {
+		if (weight <= 1) {
+			result = 0.5;
+		}
+		else if (weight <= 2) {
+			result = 0.71;
+		}
+		else if (weight <= 3) {
+			result = 0.92;
+		}
+		else {
+			result = 1.13;
+		}
+	} else if (op == "letters (metered)") {
+		if (weight <= 1) {
+			result = 0.47;
+		}
+		else if (weight <= 2) {
+			result = 0.68;
+		}
+		else if (weight <= 3) {
+			result = 0.89;
+		}
+		else {
+			result = 1.10;
+		}
+	} else if (op == "large envelopes (flats)") {
+		if (weight <= 1) {
+			result = 1.00;
+		}
+		else if (weight <= 2) {
+			result = 1.21;
+		}
+		else if (weight <= 3) {
+			result = 1.42;
+		}
+		else if (weight <= 4) {
+			result = 1.63;
+		}
+		else if (weight <= 5) {
+			result = 1.84;
+		}
+		else if (weight <= 6) {
+			result = 2.05;
+		}
+		else if (weight <= 7) {
+			result = 2.26;
+		}
+		else if (weight <= 8) {
+			result = 2.47;
+		}
+		else if (weight <= 9) {
+			result = 2.68;
+		}
+		else if (weight <= 10) {
+			result = 2.89;
+		}
+		else if (weight <= 11) {
+			result = 3.10;
+		}
+		else if (weight <= 12) {
+			result = 3.31;
+		}
+		else {
+			result = 3.52;
+		}
+	} else if (op == "first-class package service - retail") {
+    if (weight <= 4) {
+			result = 3.50;
+		}
+		else if (weight <= 8) {
+			result = 3.75;
+		}
+		else if (weight <= 9) {
+			result = 4.10;
+		}
+		else if (weight <= 10) {
+			result = 4.45;
+		}
+		else if (weight <= 11) {
+			result = 4.80;
+		}
+		else if (weight <= 12) {
+			result = 5.15;
+		}
+		else {
+			result = 5.50;
+		}
 	}
 
 	// Set up a JSON object of the values we want to pass along to the EJS result page
-	var params = {operation: op, left: left, right: right, result: result};
+	var params = {operation: op, left: left, result: result};
 
 	// Render the response, using the EJS page "result.ejs" in the pages directory
 	// Makes sure to pass it the parameters we need.
